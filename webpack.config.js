d598c3989d805e84c5684bcc61f3b02be7c8bd83
Eugene Ugrevatov <e.ugrevatov@gmail.com>
2019-08-19 00:30:47 +0300

    init config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: __dirname + '/src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./index.html" })],
+};
